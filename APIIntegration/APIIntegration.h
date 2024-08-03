bc227279c712c455faab6b214c6c5a8e1ed773e7
andjela-mircetic <andjelamircetic55@gmail.com>
2024-08-03 13:28:57 +0200

    Initial Commit

---
+++ APIIntegration/APIIntegration.h
@@ -0,0 +1,18 @@
+//
+//  APIIntegration.h
+//  APIIntegration
+//
+//  Created by Andjela Mircetic on 3.8.24..
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for APIIntegration.
+FOUNDATION_EXPORT double APIIntegrationVersionNumber;
+
+//! Project version string for APIIntegration.
+FOUNDATION_EXPORT const unsigned char APIIntegrationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APIIntegration/PublicHeader.h>
+
+
